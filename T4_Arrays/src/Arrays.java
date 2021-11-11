72d49416691ec86f9713f290f02337ae481a7d67
AlvaroSR-dev <alvarosanchezruiz3@gmail.com>
2021-11-11 11:27:30 +0100

    Arrays
    Clas 11/11/2021, arrays

---
+++ T4_Arrays/src/Arrays.java
@@ -0,0 +1,16 @@
+public class Arrays {
+    public static void main(String[] args) {
+        // array de números
+
+        int[] numeros = new int[10];
+        int[] numerosDos = {1,2,3,4,5,6,7,8,9,10};
+
+        boolean[] aciertos = new boolean[5];
+        String[] palabras = new String[3];
+
+        int longitudArray = numerosDos.length;
+        System.out.println(longitudArray);
+
+        System.out.println(numerosDos[0]);
+    }
+}
