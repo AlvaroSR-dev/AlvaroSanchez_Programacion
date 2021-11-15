694331542250d7e7b18ef0e414f1854ef8a18e58
AlvaroSR-dev <alvarosanchezruiz3@gmail.com>
2021-11-15 11:25:49 +0100

    15/11/2021

---
+++ T4_Arrays/src/Ejercicio06.java
@@ -0,0 +1,21 @@
+public class Ejercicio06 {
+    public static void main(String[] args) {
+        int[] numeros = new int[20];
+
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = (int) (Math.random() * 31);
+        }
+
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] == 6){
+                numeros[i] = 8;
+            } else if (numeros[i] == 7){
+                numeros[i] = 15;
+            } else if (numeros[i] == 20){
+                numeros[i] = 10;
+            }
+
+            System.out.println(numeros[i]);
+        }
+    }
+}
