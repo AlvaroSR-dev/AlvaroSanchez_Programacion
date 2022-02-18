141c8745282a155bbb12c71ec2b167f131a79376
MarcosSalas <92918284+MarcosSalas@users.noreply.github.com>
2022-02-18 13:02:12 +0100

    update

--- Tema06/src/Centralita/Centralita.java
+++ Tema06/src/Centralita/Centralita.java
@@ -3,27 +3,56 @@ package Centralita;
 import java.util.ArrayList;
 
 public final class Centralita {
-    protected ArrayList<Llamada> listaLlamadas;
+    private ArrayList<Llamada> listaLlamadas;
+    double costeTotal = 0;
     public Centralita(){
         listaLlamadas = new ArrayList();
     }
 
-    public void registrarLlamada(Llamada llamada){
+    public void agregarLlamada(Llamada llamada){
         listaLlamadas.add(llamada);
+        llamada.mostrarDatos();
     }
 
-    public void mostrarLlamadas(){
+    public void mostrarLlamadas(String tipo){
         for (Llamada item : listaLlamadas) {
-            item.mostrarDatos();
+            /*if (tipo.equalsIgnoreCase("Provincial")){
+                if (item instanceof LlamadaProvincial){
+                    item.mostrarDatos();
+
+                }
+            }*/
         }
     }
 
-    public double mostrarCostes(){
-        double costeTotal = 0;
+    public void listarNacionales(){
+        for (Llamada item : listaLlamadas) {
+            if(item instanceof LlamadaLocal){
+                item.mostrarDatos();
+            }
+        }
+    }
+
+    public void listarProvinciales(){
+        for (Llamada item : listaLlamadas) {
+            if(item instanceof LlamadaProvincial){
+                item.mostrarDatos();
+            }
+        }
+    }
+
+    public void listarLocales(){
+        for (Llamada item : listaLlamadas) {
+            if(item instanceof LlamadaLocal){
+                item.mostrarDatos();
+            }
+        }
+    }
+
+    public void mostrarCostes(){
         for (Llamada item: listaLlamadas) {
-            costeTotal += item.coste;
+            costeTotal += item.getCoste();
         }
         System.out.println("Coste total: "+costeTotal);
-        return costeTotal;
     }
 }
---
+++ Tema06/src/Trabajadores/Trabajadores.java
@@ -0,0 +1,5 @@
+package Trabajadores;
+
+public class Trabajadores {
+
+}
